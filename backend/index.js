c251bdae478ceca9336c1ac21fe7c0ce62da1f56
Robiul Awal Mohin <mohinr26@gmail.com>
2025-08-27 10:21:40 +0600

    banckend connected with mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,35 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
+const mongoose = require('mongoose');
+require('dotenv').config()
+
+
+
+async function main() {
+  await mongoose.connect(process.env.MONGODB_URI);
+
+
+}
+
+
+
+main().then(()=>console.log("Mongobd Connencted Succesfully!!")).catch(err => console.log(err));
+
+
+
+
+
+
+
+
+
+
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Full_stack e-commerce with_redux_state_management!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Full_stack e-commerce with_redux_state_management ${port}`)
 })
