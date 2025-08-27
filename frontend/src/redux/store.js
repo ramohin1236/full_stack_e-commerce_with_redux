9192cc0764e4707cc3473b2ac0d1a3bdccf89297
Robiul Awal Mohin <mohinr26@gmail.com>
2025-08-27 15:59:46 +0600

    store create

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,10 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit'
+import authApi from './features/auth/authApi';
 
 const store = configureStore({
   reducer: {
-    
+    [authApi.reducerPath]: authApi.reducer,
   },
+   middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(authApi.middleware),
 })
+
 
 
 export default store;
